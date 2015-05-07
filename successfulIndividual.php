823de419f176c1ee0eb09b57bc9997a3f33e3c1c
cp11@umbc.edu <chriscpaul@gmail.com>
2015-05-06 21:25:19 -0400

    made it look pretty

--- successfulIndividual.php
+++ successfulIndividual.php
@@ -1,3 +1,11 @@
+<?php
+
+	include("cssCode.html");
+	include("cssCode2.html");
+
+
+?>
+
 <html>
   <style>
     .Button
@@ -6,22 +14,51 @@
     }
     </style>
 <head>
+	<div id="security-tip">
+      <div class="content">
+
 	<P ALIGN="CENTER"><FONT SIZE="7" COLOR="RED"><U>UMBC</U></FONT>
 	<br><FONT SIZE="4">College of Engineering <br>and Information Technology</FONT>
 	</P>
-<head>
+</div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<head>	
 <body>
 	<br>
 	<br>
+	<div class="center">
 	<P ALIGN="CENTER"><FONT SIZE="6">Schedule updated!
 	<br>
 	<br>
 	Thank You</FONT>
+</div>
 	<br>
 	<br>
 	<br>
-	<a href="individualPage.php"><button type="button" class="Button" style="height:35px;background-color:red;color:white;">Return to Individual</button></a>
-	
-	<a href="adminMenu.php"><button type="button" class="Button" style="height:35px;background-color:yellow;">Main Menu</button></a>
+
+
+	<div class="fieldLogin">
+		<div class="center4">
+	<a href="individualPage.php"><button type="button" class="button go large" >Return to Individual</button></a>
+</div>
+</div>	
+
+<div class="fieldLogin">
+	<div class="center5">
+	<a href="adminMenu.php"><button type="button" class="button go large" >Main Menu</button></a>
+</div>
+</div>
+
 </body>
 </html>
