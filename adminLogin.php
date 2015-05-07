bdcb587c99e8beb704df18340dd91138721c588a
cp11@umbc.edu <chriscpaul@gmail.com>
2015-05-06 20:14:30 -0400

    now it looks pretty

--- adminLogin.php
+++ adminLogin.php
@@ -1,20 +1,83 @@
 <?php
-include("header.html");
+ include("cssCode.html");
+ include("cssCode2.html");
+
+// #example2 {
+// 	    border: 10px black;
+//     	padding: 35px;
+//     	background: #CCCCCC;
+//     	background-clip: padding-box;
+// 	}
+
 ?>
 
+
 <html>
+
+
 <body>
+
+
+
+
+ 	<div id="security-tip">
+      <div class="content">
+	<P ALIGN="CENTER"><FONT SIZE="7" COLOR="RED"><U>UMBC</U></FONT>
+	<br><FONT SIZE="4">College of Engineering <br>and Information Technology</FONT>
+	</P>
+</div>
+</div>
+
+
 <br>
-<P ALIGN="CENTER"><FONT SIZE="5"><U>Advising Administrator Log-in</U></FONT></P>
-<P ALIGN="CENTER"><FONT SIZE="3">(Enter 'password' for the Administrator Password in order to log in.)<FONT></P>
-<P ALIGN="CENTER"><FONT SIZE="3">(The log-in feature will have more functionality when connected to an accounts database.)<FONT></P>
 <br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<!-- <div class="center"> -->
+<div class="center">
+<P ALIGN="CENTER"><FONT SIZE="5"><U><big><b>Advising Administrator Log-in</b></big></U></FONT></P>	
+<P ALIGN="CENTER"><FONT SIZE="3">(Enter 'password' for the Administrator Password in order to log in.)</FONT></P>
+
+<br>
+<div class="fieldUsername">
+
 <form action='login.php' method='post' name='form1'>
-<pre><P ALIGN="CENTER"><FONT FACE="Times New Roman" SIZE = "3">Username: <input type='text' name='uname'>      Password: <input type='password' name='pass'>
+
+<pre><P ALIGN="CENTER"><FONT FACE="Times New Roman" SIZE = "3">Username: <input type='text' name='uname'>      
+</div>
+
+<!-- <div class="field2"> -->
+<div class="fieldUsername">
+<!-- <FONT FACE="Times New Roman" SIZE = "3">Password: <input type='password1' name='pass'> -->
+<pre><P ALIGN="CENTER"><FONT FACE="Times New Roman" SIZE = "3">Password: <input type='password1' name='pass'>
+</div>
+
+<!-- <div class="field2"> -->
+<center>Administrator Password: <big><input type='password' name='adminPass'></big></center>
+<!-- </div> -->
+
 <br>
-Administrator Password: <input type='password' name='adminPass'></FONT>
 <br>
-<input type='submit' value="Log in" style="width:100px;height:60px;background-color:black;color:yellow;"> </P></pre>
+
+
+<!--<input type='submit' value="Log in" style="width:100px;height:60px;background-color:black;color:yellow;"> </P></pre> -->
+<!-- <div class="fieldLogin"> -->
+<center><input type='submit' value="Log in" class="button go large"></center>
+<!-- </div> -->
+
+
+<!-- </div>
+ -->
 </form>
 </body>
 </html>
