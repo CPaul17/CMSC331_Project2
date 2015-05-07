8cbcdb11a105f0cd7e9e1db88bd4116dce6fabfd
cp11@umbc.edu <chriscpaul@gmail.com>
2015-05-06 20:17:32 -0400

    looks pretty now

--- adminMenu.php
+++ adminMenu.php
@@ -1,25 +1,66 @@
 <?php
-	include("header.html");
+	// include("header.html");
+ 	
+	include("cssCode.html")
 ?>
+
 
 <html>
 <body>
+
+	<div id="security-tip">
+      <div class="content">
+	<P ALIGN="CENTER"><FONT SIZE="7" COLOR="RED"><U>UMBC</U></FONT>
+	<br><FONT SIZE="4">College of Engineering <br>and Information Technology</FONT>
+	</P>
+	</div>
+	</div>
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+
 	<br>
 	<br>
 	<P ALIGN="CENTER"><FONT SIZE="6"><U>Main Menu</U></FONT>
-	
+	<br>	
+
 	<br>
 	<br>
-	<a href="individualPage.php"><button type="button" class="Button" style="height:50px;width:150px;background-color:black;color:yellow;">Schedule Individual</button></a>
+
+<!-- 	<a href="individualPage.php"><button type="button" class="button go large">Schedule Individual</button></a> -->
+	<a href="individualPage.php"><button type="button" class="Button" style="height:50px;width:200px;background-color: #66AA44;color:#FFF;border-radius: 8px;"><big><b>Schedule Individual</b></big></button></a>
+
 	<br>	
 	<br>
-	<a href="groupPage.php"><button type="button" class="Button" style="height:50px;width:150px;background-color:black;color:yellow;">Schedule Group</button></a>
+	<br>	
+	<br>
+
+<!-- 	<a href="groupPage.php"><button type="button" class="button go large">Schedule Group</button></a> -->
+	<a href="groupPage.php"><button type="button" class="Button" style="height:50px;width:200px;background-color: #66AA44;color:#FFF;border-radius: 8px"><big><b>Schedule Group</b></big></button></a>
+
 	<br>
 	<br>
-	<a href="addAdvisor.php"><button type="button" class="Button" style="height:50px;width:150px;background-color:black;color:yellow;">Add Advisors</button></a>
+	<br>	
+	<br>	
+
+<!-- 	<a href="addAdvisor.php"><button type="button" class="button go large" >Add Advisors</button></a> -->
+	<a href="addAdvisor.php"><button type="button" class="Button" style="height:50px;width:200px;background-color: #66AA44;color:#FFF;border-radius: 8px"><big><b>Add Advisors</b></big></button></a>
+	<br>	
 	<br>
 	<br>
-	<a href="printingPage.php"><button type="button" class="Button" style="height:50px;width:150px;background-color:black;color:yellow;">Print Schedule</button></a>
+	<br>
+
+<!-- 	<a href="printingPage.php"><big><button type="button" class="button go large" >Print Schedule</button></big></a> -->
+	<a href="printingPage.php"><big><button type="button" class="Button" style="height:50px;width:200px;background-color: #66AA44;color:#FFF;border-radius: 8px" ><big><b>Print Schedule</b></big></button></big></a>
+
 	</P>	
 </body>
 </html>
